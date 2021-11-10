33f5b5010378516e37c270a14e88b3dc53103e55
[Jadgesh Inderjeet] <[jadgesh.i@gmail.com]>
2021-11-09 20:04:26 -0500

    Added features from EP05

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private PlayerMovement movement;
+
+    private void Start(){
+        movement = GetComponent<PlayerMovement>();
+    }
+    void OnCollisionEnter(Collision other){
+        if(other.collider.tag == "Obstacle"){
+            movement.enabled = false;
+            Debug.Log("Hit Obstacle");
+        }
+    }
+}
