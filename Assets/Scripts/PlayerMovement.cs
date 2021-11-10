4966a4c91c5464fe2561b985e13c0c4e64b28abb
[Jadgesh Inderjeet] <[jadgesh.i@gmail.com]>
2021-11-09 19:43:39 -0500

    Added features from EP03

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ public class PlayerMovement : MonoBehaviour{
 
     private Rigidbody rb;
 
+    [SerializeField]
+    private float forwardForce = 2000f;
+
+    [SerializeField]
+    private float sidewaysForce = 500f;
+
     // Start is called before the first frame update
     void Start(){
         rb = GetComponent<Rigidbody>();
@@ -11,6 +17,14 @@ public class PlayerMovement : MonoBehaviour{
 
     // Update is called once per frame
     void FixedUpdate(){
-        rb.AddForce(0,0,2000 * Time.deltaTime);
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
+        }
+
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0);
+        }
     }
 }
